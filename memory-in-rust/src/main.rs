1b90f958a91423af03a5479fcf72de379b9be4cb
silver <jcarlos.perez.torres1@gmail.com>
2024-06-16 07:23:40 -0600

    step 1

--- memory-in-rust/src/main.rs
+++ memory-in-rust/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+    let rows: i32 = 6;
+    let cols: i32 = 6;
+    let mut board = [[mut char; cols]; rows];
     println!("Bienvenido a memorama!");
+    for _ in 1..rows {
+        for _ in 1..cols {
+            print!("@ ");
+        }
+        println!();
+    }
 }
